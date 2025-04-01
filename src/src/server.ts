783e546ebd44c34b37b36d4b502979d7e43db30c
supertren <43068656+supertren@users.noreply.github.com>
2025-04-01 02:25:38 +0200

    Update server.ts

--- src/src/server.ts
+++ src/src/server.ts
@@ -59,7 +59,7 @@ app.get('**', (req, res, next) => {
  */
 if (isMainModule(import.meta.url)) {
   const port = process.env['PORT'] || 4000;
-  app.listen(port, '0.0.0.0', () => {
+  app.listen(port, '0.0.0.0') => {
     console.log(`Node Express server listening on http://0.0.0.0:${port}`);
   });
 }
